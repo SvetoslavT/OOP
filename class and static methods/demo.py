1af03d2826c9fae7289523333978e82b65bdc29e
Svetoslav Trifonov <76935379+SvetoslavT@users.noreply.github.com>
2022-08-12 10:30:10 +0300

    Add files via upload

---
+++ class and static methods/demo.py
@@ -0,0 +1,56 @@
+# class Calculator:
+#     @staticmethod
+#     def add(*args):
+#         pass
+# 
+#     @staticmethod
+#     def multiply(*args):
+#         pass
+# 
+#     @staticmethod
+#     def divide(*args):
+#         pass
+# 
+#     @staticmethod
+#     def subtract(*args):
+#         pass
+# class Pizza:
+#
+#     def __init__(self, ingredients):
+#         self.ingredients = ingredients
+#
+#     @classmethod
+#     def create(cls):
+#         return cls(['cheese', 'tomato sauce', 'peperoni'])
+#
+# pizza1 = Pizza.create()
+# print(pizza1.ingredients)
+
+# class Shop:
+#
+#     @classmethod
+#     def small_shop(cls, name: str, type: str):
+#         pass
+#
+#     def add_item(self, item_name: str):
+#         pass
+#
+#     def remove_item(self, item_name: str, amount: int):
+#         pass
+#
+#     def __repr__(self):
+#         pass
+#
+# class Integer:
+#
+#     @classmethod
+#     def from_float(cls, value):
+#         pass
+#
+#     @classmethod
+#     def from_roman(cls, value):
+#         pass
+#
+#     @classmethod
+#     def from_string(cls, value):
+#         pass
